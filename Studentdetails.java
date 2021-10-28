e0b84d1110b963a7b6bc0505c417fa685c4e3d73
VishwanathShidramayyanamath <vishu.smath@gmail.com>
2021-10-28 09:10:44 +0530

    adding to newbranch

---
+++ Studentdetails.java
@@ -0,0 +1,17 @@
+class Programstd
+{
+    public static void main(String[] args) {
+         
+        String studentName="Premlinga",Batch="B1";
+        int USN=419826,CGPA=9;
+        char section='B';
+
+      
+
+        System.out.println("student Name is\t" +studentName); 
+        System.out.println("student Batch is\t" +Batch);
+        System.out.println("student USN is  \t" +USN);
+        System.out.println("student CGPA is\t" +CGPA);
+        System.out.println("student section is \t" +section);
+    }
+}
